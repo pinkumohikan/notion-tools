620d32d10f11439aa1524ff31cf68d77d337a391
Hokuto Shinoda <pinkumohikan+github@gmail.com>
2023-10-31 00:43:29 +0900

    chore: 月間の合計稼働時間を求めるツールを実装

---
+++ working-matome/src/index.ts
@@ -0,0 +1,46 @@
+const { Client } = require("@notionhq/client");
+const { DateTime } = require('luxon');
+
+const main = async () => {
+  const notion = new Client({
+    auth: process.env.NOTION_API_TOKEN,
+  });
+
+  const baseDate = DateTime.now().setZone('Asia/Tokyo');
+  const firstDay = baseDate.startOf('month');
+  const finalDay = baseDate.endOf('month');
+
+  console.log(`集計期間: ${firstDay.toFormat('yyyy-MM-dd')} ~ ${finalDay.toFormat('yyyy-MM-dd')}`)
+
+  const r = await notion.databases.query({
+    database_id: process.env.NOTION_DAYBOOK_DB_ID,
+    filter: {
+      and: [
+        {
+          property: "作成日時",
+          date: {
+            on_or_after: `${firstDay.toFormat('yyyy-MM-dd')}`
+          },
+        },
+        {
+          property: "作成日時",
+          date: {
+            before: `${finalDay.toFormat('yyyy-MM-dd')}`
+          },
+        },
+      ],
+    },
+  });
+
+  const totalWorkedMinutes = r.results
+    .map((page: any) => {
+      return page.properties["稼働時間"].formula.number;
+    })
+    .reduce((total: number, n: number) => total + n, 0);
+
+  const workedHours = Math.floor(totalWorkedMinutes / 60);
+  const workedMinutes = totalWorkedMinutes % 60;
+  console.log(`今月の稼働時間 ${workedHours}:${workedMinutes}`);
+};
+
+main();
